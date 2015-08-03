eedacbacb6f9e8eca12a998b7de5fba6a1879d33
Kurt Nelson <kurtisnelson@gmail.com>
2015-08-03 10:26:52 -0500

    Test registration w dupe email

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,17 +3,29 @@ require 'request_helper'
 describe UsersController do
   describe 'POST /users' do
     it 'allows registration' do
-      password = SecureRandom.hex
-      email = Faker::Internet.email
-
       payload = {
         user: {
-          email: email,
-          password: password
+          email: Faker::Internet.email,
+          password: SecureRandom.hex
         }
       }
       post users_path, payload
       expect(response).to have_http_status(:success)
+      expect(json["data"]["id"]).to_not be_empty
+    end
+
+    it 'prevents dupe email' do
+      user = FactoryGirl.create :user
+
+      payload = {
+        user: {
+          email: user.email,
+          password: SecureRandom.hex
+        }
+      }
+      post users_path, payload
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json["errors"]["email"]).to_not be_empty
     end
   end
 end
