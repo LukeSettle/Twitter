8bf01ec742eddb74adb62279c1ddcf97fec80fa0
Luke Settle <lukewestonsettle@gmail.com>
2015-08-07 13:49:29 -0500

    added sort

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,5 @@ class Tweet < ActiveRecord::Base
 	belongs_to :user
 	validates :body, length: {minimum: 5, maximum: 200}
   validates :user, presence: true
+  default_scope order('created_at DESC')
 end
